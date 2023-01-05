7e5b861e8a08ab86ce0f2299781875ee0ed6bf97
Jrsergio00 <junior.sergio@escolar.ifrn.edu.br>
2023-01-05 14:41:34 -0300

    Create filme

--- code/php/cadastrarCINEFILO.php
+++ code/php/cadastrarCINEFILO.php
@@ -5,8 +5,10 @@ $nome = filter_input(INPUT_POST, 'nome');
 $email = filter_input(INPUT_POST, 'email');
 
 $sql = $pdo->prepare("INSERT INTO cinefilo (nome, email) VALUES (:nome, :email)");
+
 $sql->bindValue(':nome', $nome);
 $sql->bindValue(':email', $email);
+
 $sql->execute();
 
 header("Location: ../index.html");
